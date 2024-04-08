a90a3b07ef5e9ebbb316675b9ae2d113eb69e635
Torsten <webfalter@googlemail.com>
2024-04-08 16:11:01 +0200

    Update Liga.php

--- src/lib/Liga.php
+++ src/lib/Liga.php
@@ -1,8 +1,32 @@
 <?php
 
-namespace LMOLib;
+declare(strict_types=1);
+/**
+ * Liga
+ *
+ * Bildet das gesammte LigaFile als Objekt ab
+ *
+ * @author Tim Schumacher
+ * @author Rene Marth
+ * @author Markus Doerfling
+ * @author Marcus Schug
+ * @author Torsten Hofmann
+ *
+ * @package LMOLib
+ * @version 5.0.1
+ *
+ */
+namespace LMOLib\lib;
 
-class Liga
-{
+use LMOLib\lib\Team;
+use LMOLib\lib\Partie;
+use LMOLib\lib\Spieltag;
+use LMOLib\inc\optionsSektion;
+
+class Liga {
+   /**
+    * @const string
+    */
+    const VERSION = '5.0.1';
 
 }
