d46f4f6f17b7e59c5cdd872ec7b54dcfa8ae558d
Torsten <webfalter@googlemail.com>
2024-04-08 16:14:30 +0200

    Update Team.php

--- src/lib/Team.php
+++ src/lib/Team.php
@@ -1,6 +1,23 @@
 <?php
 
-namespace LMOLib;
+declare(strict_types=1);
+/**
+ * LMOLib
+ *
+ *
+ * Mannschaft (Team), die an einer Liga teilnimmt
+ *
+ * @author Tim Schumacher
+ * @author Rene Marth
+ * @author Markus Doerfling
+ * @author Marcus Schug
+ * @author Torsten Hofmann
+ *
+ * @package LMOLib
+ * @version 5.0.1
+ *
+ */
+namespace LMOLib\lib;
 
 class Team
 {
