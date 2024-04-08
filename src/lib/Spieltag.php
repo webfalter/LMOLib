3322e6574a22bb3edebef145082e2f247a990302
Torsten <webfalter@googlemail.com>
2024-04-08 16:13:50 +0200

    Update Spieltag.php

--- src/lib/Spieltag.php
+++ src/lib/Spieltag.php
@@ -1,6 +1,24 @@
 <?php
 
-namespace LMOLib;
+declare(strict_types=1);
+/**
+ * LMOLib
+ *
+ * Repraesentiert einen Spieltag
+ * - Eine Liga hat mehrere Spieltage.
+ * - An einem Spieltag finden mehrere Partien statt.
+ *
+ * @author Tim Schumacher
+ * @author Rene Marth
+ * @author Markus Doerfling
+ * @author Marcus Schug
+ * @author Torsten Hofmann
+ *
+ * @package LMOLib
+ * @version 5.0.1
+ *
+ */
+namespace LMOLib\lib;
 
 class Spieltag
 {
