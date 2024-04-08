ce7e6f33e3d4f9ae52817ef04bdf723d0478f556
Torsten <webfalter@googlemail.com>
2024-04-08 16:12:22 +0200

    Update Partie.php

--- src/lib/Partie.php
+++ src/lib/Partie.php
@@ -1,6 +1,22 @@
 <?php
 
-namespace LMOLib;
+declare(strict_types=1);
+/**
+ * Liga
+ *
+ * Partie, die in einer Liga gespielt wird
+ *
+ * @author Tim Schumacher
+ * @author Rene Marth
+ * @author Markus Doerfling
+ * @author Marcus Schug
+ * @author Torsten Hofmann
+ *
+ * @package LMOLib
+ * @version 5.0.1
+ *
+ */
+namespace LMOLib\lib;
 
 class Partie
 {
