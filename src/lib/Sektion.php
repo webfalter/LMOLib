86f217e5cdde2a9a83aa6292adaee95048ff5333
Torsten <webfalter@googlemail.com>
2024-04-08 16:13:08 +0200

    Update Sektion.php

--- src/lib/Sektion.php
+++ src/lib/Sektion.php
@@ -1,6 +1,22 @@
 <?php
 
-namespace LMOLib;
+declare(strict_types=1);
+/**
+ * LMOLib
+ *
+ * Sektionen eines LigFiles zB. [Round1]
+ *
+ * @author Tim Schumacher
+ * @author Rene Marth
+ * @author Markus Doerfling
+ * @author Marcus Schug
+ * @author Torsten Hofmann
+ *
+ * @package LMOLib
+ * @version 5.0.1
+ *
+ */
+namespace LMOLib\lib;
 
 class Sektion
 {
