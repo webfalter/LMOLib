e3a7bd097b24a9bd8a1606c4dc2cb6656851613a
Torsten <webfalter@googlemail.com>
2024-04-09 12:33:40 +0200

    Update Helper.php

--- src/helper/Helper.php
+++ src/helper/Helper.php
@@ -1,14 +1,54 @@
 <?php
+
+declare(strict_types=1);
+/**
+ * Liga
+ *
+ * Hilfsfunktionen zum Objekt Liga
+ *
+ * @author Tim Schumacher
+ * @author Rene Marth
+ * @author Markus Doerfling
+ * @author Marcus Schug
+ * @author Torsten Hofmann
+ *
+ * @package LMOLib
+ *
+ * @version 5.0.1
+ *
+ */
 namespace LMOLib\helper;
 
-interface Helper
+class Helper
 {
-/**
-	*
-	*/
-  function get_test() {
-    echo "test";
-  
-  }
+    
+    public function __construct() {}
+    
+    /**
+     * <br> getLigaObject
+     * <br> die Funktion sucht den Parameter LigaType im Ligafile und gibt passend hierzu ein
+     * <br> Object der Klassen liga oder einer der Kindklassen zurück
+     *
+     * @version 1.0
+     * @since 2.7
+     * @param string file Pfad zur Ligadatei (*.l98)
+     * @return object ein object passend zum Ligafile
+     */
+    public function getLigaObject($file) {
+        if (file_exists($file) ) {
+            $ligaFile = file($file);
+            foreach ($ligaFile as $configLine) {
+                if (preg_match("/^LigaType=([^\n]+)/",$configLine,$ligaType) ) {
+                    break;
+                }
+            }
+            $ligaType[1] .= "Liga";
+            $ligaNewType = '\\LMOLib\\lib\\'.$ligaType[1];
+            if (class_exists($ligaNewType))
+                return new $ligaNewType();
+            else return new \LMOLib\lib\Liga();
+        } else return false;
+        
+    }
   
 }
