071248901c240ec40b08266916a245aa5cc70adc
Torsten <webfalter@googlemail.com>
2024-04-10 11:22:29 +0200

    Update fussballLiga.php

--- src/inc/fussballLiga.php
+++ src/inc/fussballLiga.php
@@ -1,1 +1,29 @@
+<?php
+/**
+ * LMOLib\inc\fussballLiga
+ *
+ * Bildet eine Fussball Liga als Objekt ab
+ *
+ * @author Tim Schumacher
+ * @author Rene Marth
+ * @author Markus Doerfling
+ * @author Marcus Schug
+ * @author Torsten Hofmann
+ *
+ * @package LMOLib
+ *
+ * @version 5.0.1
+ *
+ */
+declare(strict_types=1);
 
+namespace LMOLib\inc;
+
+class fussballLiga extends \LMOLib\lib\Liga
+{
+/**
+ *
+ */
+  const Ligatype = "Fussball";
+  
+} // END class fussballLiga
