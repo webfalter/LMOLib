3807b5801a53da2326252535f431758ab09ec8f0
Torsten <webfalter@googlemail.com>
2024-04-10 11:23:48 +0200

    Update basketballLiga.php

--- src/inc/basketballLiga.php
+++ src/inc/basketballLiga.php
@@ -1,1 +1,29 @@
+<?php
+/**
+ * LMOLib\inc\basketballLiga
+ *
+ * Bildet eine Basketball Liga als Objekt ab
+ *
+ * @author Tim Schumacher
+ * @author Rene Marth
+ * @author Markus Doerfling
+ * @author Marcus Schug
+ * @author Torsten Hofmann
+ *
+ * @package LMOLib
+ *
+ * @version 5.0.1
+ *
+ */
+declare(strict_types=1);
 
+namespace LMOLib\inc;
+
+class bassketballLiga extends \LMOLib\lib\Liga
+{
+/**
+ *
+ */
+  const Ligatype = "Basketball";
+  
+} // END class basketballLiga
