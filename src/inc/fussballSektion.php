a9b396b0a461023bcaf87bb7cfa895e2dbbcf886
Torsten <webfalter@googlemail.com>
2024-04-10 11:49:27 +0200

    Update fussballSektion.php

--- src/inc/fussballSektion.php
+++ src/inc/fussballSektion.php
@@ -1,1 +1,28 @@
+<?php
+/**
+ * LMOLib\inc\fussballSektion
+ *
+ * Sektionen einer fusballLiga zB. [Round1]
+ *
+ * @author Tim Schumacher
+ * @author Rene Marth
+ * @author Markus Doerfling
+ * @author Marcus Schug
+ * @author Torsten Hofmann
+ *
+ * @package LMOLib
+ *
+ * @version 5.0.1
+ *
+ */
+declare(strict_types=1);
 
+namespace LMOLib\inc;
+
+class fussballSektion extends \LMOAPI\lib\Sektion
+{
+/**
+ *
+ */
+  
+} // END class fussballSektion
