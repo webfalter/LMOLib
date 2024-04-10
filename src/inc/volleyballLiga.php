13072221866d9a29d7338a2d862a0b14f9d546a2
Torsten <webfalter@googlemail.com>
2024-04-10 11:25:50 +0200

    Update volleyballLiga.php

--- src/inc/volleyballLiga.php
+++ src/inc/volleyballLiga.php
@@ -1,1 +1,30 @@
+<?php
+/**
+ * LMOLib\inc\volleyballLiga
+ *
+ * Bildet eine Volleyball Liga als Objekt ab
+ *
+ * @author Tim Schumacher
+ * @author Rene Marth
+ * @author Markus Doerfling
+ * @author Marcus Schug
+ * @author Torsten Hofmann
+ *
+ * @package LMOLib
+ *
+ * @version 5.0.1
+ *
+ */
+declare(strict_types=1);
+
+namespace LMOLib\inc;
+
+class volleyballLiga extends \LMOLib\lib\Liga
+{
+/**
+ *
+ */
+  const Ligatype = "Volleyball";
+  
+} // END class volleyballLiga
 
