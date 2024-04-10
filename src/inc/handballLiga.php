960b4abc51f30e79b228838d8c258f3aa13217f0
Torsten <webfalter@googlemail.com>
2024-04-10 11:24:45 +0200

    Update handballLiga.php

--- src/inc/handballLiga.php
+++ src/inc/handballLiga.php
@@ -1,1 +1,29 @@
+<?php
+/**
+ * LMOLib\inc\handballLiga
+ *
+ * Bildet eine Handball Liga als Objekt ab
+ *
+ * @author Tim Schumacher
+ * @author Rene Marth
+ * @author Markus Doerfling
+ * @author Marcus Schug
+ * @author Torsten Hofmann
+ *
+ * @package LMOLib
+ *
+ * @version 5.0.1
+ *
+ */
+declare(strict_types=1);
 
+namespace LMOLib\inc;
+
+class handballLiga extends \LMOLib\lib\Liga
+{
+/**
+ *
+ */
+  const Ligatype = "Handball";
+  
+} // END class handballLiga
