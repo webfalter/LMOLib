fb820d1f33012c0dfa58a53910492428ca69a1e6
Torsten <webfalter@googlemail.com>
2024-04-10 11:27:06 +0200

    Update kegelLiga.php

--- src/inc/kegelLiga.php
+++ src/inc/kegelLiga.php
@@ -1,1 +1,29 @@
+<?php
+/**
+ * LMOLib\inc\kegelLiga
+ *
+ * Bildet eine Kegel Liga als Objekt ab
+ *
+ * @author Tim Schumacher
+ * @author Rene Marth
+ * @author Markus Doerfling
+ * @author Marcus Schug
+ * @author Torsten Hofmann
+ *
+ * @package LMOLib
+ *
+ * @version 5.0.1
+ *
+ */
+declare(strict_types=1);
 
+namespace LMOLib\inc;
+
+class kegelLiga extends \LMOLib\lib\Liga
+{
+/**
+ *
+ */
+  const Ligatype = "Kegeln";
+  
+} // END class kegelLiga
