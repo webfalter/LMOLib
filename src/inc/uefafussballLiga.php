02a6ef5eec58ec3804880303b548bd2bf7199dc1
Torsten <webfalter@googlemail.com>
2024-06-19 16:54:51 +0200

    Create uefafussballLiga.php

---
+++ src/inc/uefafussballLiga.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * LMOLib\inc\uefafussballLiga
+ *
+ * Bildet eine UefaFussball Liga als Objekt ab
+ * Uefa Modus 2024
+ *
+ * @author Tim Schumacher
+ * @author Rene Marth
+ * @author Markus Doerfling
+ * @author Marcus Schug
+ * @author Torsten Hofmann
+ *
+ * @package LMOLib
+ *
+ * @version 5.0.1
+ *
+ */
+declare(strict_types=1);
+
+namespace LMOLib\inc;
+
+class uefafussballLiga extends \LMOLib\lib\Liga
+{
+/**
+ *
+ */
+  const Ligatype = "UefaFussball";
+  
+} // END class uefafussballLiga
